2cd02f8c55238582cc83322964d06d5ce8802dac
priyanshu <ginkalpriyanshu@gmail.com>
2024-02-14 11:02:07 +0530

    [Priyanshu] Add: Added Functionality to test the Networking Service

---
+++ NetworkUtilityTests/PingTests/NetworkServiceTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using NetworkUtility.Ping;
+
+namespace NetworkUtilityTests.PingTests
+{
+    public class NetworkServiceTests
+    {
+        [Fact]
+        public void NetworkService_SendPing_ReturnString()
+        {
+            // Arrange
+            var pingService = new NetworkService();
+
+            // Act
+            var result = pingService.SendPing();
+
+            // Assert
+            result.Should().NotBeNullOrWhiteSpace();
+            result.Should().Be("Success: Ping Sent!");
+            result.Should().Contain("Success", Exactly.Once());
+        }
+    }
+}
