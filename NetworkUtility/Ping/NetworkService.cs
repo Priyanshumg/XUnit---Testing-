3f181b30b9b141b2234aa481cd0ae8a96f098744
priyanshu <ginkalpriyanshu@gmail.com>
2024-02-14 11:30:44 +0530

    [Priyanshu] Add: Added Functionality to return values in our PingTimeout Method

--- NetworkUtility/Ping/NetworkService.cs
+++ NetworkUtility/Ping/NetworkService.cs
@@ -14,5 +14,10 @@ namespace NetworkUtility.Ping
             // Build Packets()
             return "Success: Ping Sent!";
         }
+
+        public int PingTimeout(int a, int b)
+        {
+            return a + b;
+        }
     }
 }
