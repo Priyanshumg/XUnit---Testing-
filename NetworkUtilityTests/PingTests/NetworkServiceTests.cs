10886184b11b87920c9f945a0aec97fbe4f78809
priyanshu <ginkalpriyanshu@gmail.com>
2024-02-14 12:54:35 +0530

    [Add] Add: Added Testing Elements and ready to start Testing

--- NetworkUtilityTests/PingTests/NetworkServiceTests.cs
+++ NetworkUtilityTests/PingTests/NetworkServiceTests.cs
@@ -5,6 +5,7 @@ using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 using FluentAssertions;
+using FluentAssertions.Extensions;
 using NetworkUtility.Ping;
 
 namespace NetworkUtilityTests.PingTests
@@ -50,5 +51,17 @@ namespace NetworkUtilityTests.PingTests
             result.Should().NotBeInRange(-1000, 0);
         }
 
+        [Fact]
+        public void NetworkService_LastPingDate_ReturnsDateTime()
+        {
+            // Arrange
+
+            // Act
+            DateTime result = _pingService.LastPingDate();
+
+            // Assert
+            result.Should().BeAfter(1.January(2020));
+            result.Should().BeBefore(1.January(2025));
+        }
     }
 }
